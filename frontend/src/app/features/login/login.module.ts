f6d1fa11bbb4e97fc4544c1b9435a02cf818a017
jkanimea <jkanimea@gmail.com>
2026-06-14 14:19:57 +1200

    Update web GSI client_id to encounter-daily Firebase project

--- frontend/src/app/features/login/login.module.ts
+++ frontend/src/app/features/login/login.module.ts
@@ -213,7 +213,7 @@ export class LoginPage implements OnDestroy {
         await this.loadScript('https://accounts.google.com/gsi/client');
       }
       window.google!.accounts.id.initialize({
-        client_id: '126956037492-0v2i92mj4q0ulko5u5io1bd5do619liu.apps.googleusercontent.com',
+        client_id: '868571551367-kkm4ggn0d9cc457k6s0p9rhoipq1bkio.apps.googleusercontent.com',
         callback: (response: GoogleCredentialResponse) => {
           if (response?.credential) {
             this.googleCredResolve?.(response.credential);
